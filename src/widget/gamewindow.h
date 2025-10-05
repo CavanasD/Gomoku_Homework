fbe8bdb8b4ea5857a17e808a0049ce756907e7ea
CavanasD <m18404288789@163.com>
2025-10-05 14:00:38 +0800

    实现人人对战

--- src/widget/gamewindow.h
+++ src/widget/gamewindow.h
@@ -2,6 +2,7 @@
 #define MY_APP_GAMEWINDOW_H
 
 #include <QWidget>
+#include <QMouseEvent>
 
 
 QT_BEGIN_NAMESPACE
@@ -14,6 +15,12 @@ QT_END_NAMESPACE
 
 class gameWindow : public QWidget {
     Q_OBJECT
+protected:
+    void paintEvent(QPaintEvent *Event) override;
+    void mouseMoveEvent(QMouseEvent *Event) override;
+    void mousePressEvent(QMouseEvent *Event) override;
+    void updateTurnLabel() const;
+    bool checkWin(int x, int y);
 
 public:
     explicit gameWindow(QWidget *parent = nullptr);
@@ -21,8 +28,19 @@ public:
     ~gameWindow() override;
 private slots:
     void on_backToMain_clicked();
+    void on_startBut_clicked();
+    void on_resetBut_clicked();
+
+
 private:
     Ui::gameWindow *ui;
+
+    // 成员变量
+    int chess[15][15]{};
+    bool isPlayerOneTurn;
+    bool gameActive;
+    int moveX, moveY{};
+    int lastMoveX{}, lastMoveY{};
 };
 
 
---
+++ src/widget/ui_gamewindow.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'gamewindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_GAMEWINDOW_H
+#define UI_GAMEWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_gameWindow
+{
+public:
+    QPushButton *backToMain;
+    QPushButton *startBut;
+    QPushButton *resetBut;
+    QLabel *label;
+    QLabel *whoTurn;
+    QLabel *whoWin;
+
+    void setupUi(QWidget *gameWindow)
+    {
+        if (gameWindow->objectName().isEmpty())
+            gameWindow->setObjectName("gameWindow");
+        gameWindow->resize(786, 704);
+        backToMain = new QPushButton(gameWindow);
+        backToMain->setObjectName("backToMain");
+        backToMain->setGeometry(QRect(640, 20, 111, 31));
+        startBut = new QPushButton(gameWindow);
+        startBut->setObjectName("startBut");
+        startBut->setGeometry(QRect(640, 60, 111, 31));
+        resetBut = new QPushButton(gameWindow);
+        resetBut->setObjectName("resetBut");
+        resetBut->setGeometry(QRect(640, 100, 111, 31));
+        label = new QLabel(gameWindow);
+        label->setObjectName("label");
+        label->setGeometry(QRect(620, 150, 141, 51));
+        QFont font;
+        font.setFamilies({QString::fromUtf8("\346\261\211\344\273\252\351\273\221\347\245\236\350\257\235")});
+        font.setPointSize(24);
+        label->setFont(font);
+        whoTurn = new QLabel(gameWindow);
+        whoTurn->setObjectName("whoTurn");
+        whoTurn->setGeometry(QRect(630, 210, 141, 51));
+        whoTurn->setFont(font);
+        whoWin = new QLabel(gameWindow);
+        whoWin->setObjectName("whoWin");
+        whoWin->setGeometry(QRect(280, 600, 291, 51));
+        whoWin->setFont(font);
+
+        retranslateUi(gameWindow);
+
+        QMetaObject::connectSlotsByName(gameWindow);
+    } // setupUi
+
+    void retranslateUi(QWidget *gameWindow)
+    {
+        gameWindow->setWindowTitle(QCoreApplication::translate("gameWindow", "gameWindow", nullptr));
+        backToMain->setText(QCoreApplication::translate("gameWindow", "\303\227", nullptr));
+        startBut->setText(QCoreApplication::translate("gameWindow", "\345\274\200\345\247\213\346\270\270\346\210\217", nullptr));
+        resetBut->setText(QCoreApplication::translate("gameWindow", "\351\207\215\347\275\256\346\243\213\347\233\230", nullptr));
+        label->setText(QCoreApplication::translate("gameWindow", "\345\275\223\345\211\215\345\233\236\345\220\210\357\274\232", nullptr));
+        whoTurn->setText(QCoreApplication::translate("gameWindow", "\351\273\221\346\243\213", nullptr));
+        whoWin->setText(QCoreApplication::translate("gameWindow", "\344\275\240\350\203\275\346\211\223\350\264\245\346\265\201\350\220\244\345\220\227", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class gameWindow: public Ui_gameWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_GAMEWINDOW_H
